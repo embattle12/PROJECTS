db5e7a4b49b6b743fc92840ec7cb80258de34315
Ajinkya Kshirsagar <78659761+embattle12@users.noreply.github.com>
2021-02-25 21:16:03 +0530

    Add files via upload
    A basic python built game of rock paper scissors. It was a fun ride building the game. i am getting into programing.

---
+++ ROCK PAPER SCISSORS.py
@@ -0,0 +1,16 @@
+import random
+def play():
+    user = input("Whats your choice? 'r' for rock, 'p' for paper, 's' for scissors \n")
+    computer = random.choice(['r', 'p', 's'])
+
+    if user == computer:
+        return 'It\'s a tie'
+    if is_win(user, computer):
+        return "You won!"
+    return "You lost!"    
+def is_win(player, opponent):
+    #return true is player wins
+    if (player == 'r' and opponent == 's') or (player =='s' and opponent == 'p')\
+        or (player == 'p' and opponent is 'r'):
+        return True
+print(play())
